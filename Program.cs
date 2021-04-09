1ef86f7884156d698017e0af7937cd236248d317
JonBarron93 <machin3x7@gmail.com>
2021-04-09 14:06:17 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Exercise
+{
+    class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            if (c == true)
+            {
+                if (a + b == 1)
+                {
+                    return $"{ a + b } dollar";
+                }
+                else
+                    return $"{ a + b } dollars";
+            }
+            else
+                return $"{ a + b }";
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
